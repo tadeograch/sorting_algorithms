87d343be753b259fccb4f151f9c7a4a5d3a5bf6f
Tadeo Grach <tade.g.17@gmail.com>
2021-02-01 20:45:33 +0000

    fixing task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,67 +1,67 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts doubly linked list of integers, ascending order
+ * insertion_sort_list - Implements insertion sort algorithm
  *
- * @list: The list to be sorted
- */
+ * @list: The doubly linked list to be sorted
+*/
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp_n, *temp_sort, *swapper;
 	int len = dlistint_len(*list);
 
+	if (!list || *list == NULL)
+		return;
+
 	if (len < 2)
 		return;
-	if (len > 2)
+
+	temp_n = *list;
+	if (len == 2)
 	{
-		temp_n = *list;
-		while (temp_n != NULL)
+		temp_n = temp_n->next;
+		if (temp_n->n < (temp_n->prev)->n)
 		{
-			temp_sort = temp_n;
-			if (temp_sort->prev != NULL)
+			swap(temp_n, temp_n->prev, list);
+			print_list(*list);
+		}
+		return;
+	}
+	while (temp_n != NULL)
+	{
+		temp_sort = temp_n;
+		if (temp_sort->prev != NULL)
+		{
+			while (temp_sort->n < (temp_sort->prev)->n && temp_sort->prev != NULL)
 			{
-				while (temp_sort->n < (temp_sort->prev)->n && temp_sort->prev != NULL)
+				swapper = temp_sort->prev;
+				if (swapper->prev == NULL)
 				{
-					swapper = temp_sort->prev;
-					if (swapper->prev == NULL)
-					{
-						swap(temp_sort, swapper, list);
-						print_list(*list);
-						break;
-					}
 					swap(temp_sort, swapper, list);
 					print_list(*list);
+					break;
 				}
-			}
-			temp_n = temp_n->next;
-		}
-	}
-	else if (len == 2)
-	{
-		temp_n = *list;
-		if (temp_n->n >= (temp_n->next)->n)
-		{
-				swap(temp_n->next, temp_n, list);
+				swap(temp_sort, swapper, list);
 				print_list(*list);
+			}
 		}
+		temp_n = temp_n->next;
 	}
 }
 
 /**
- * swap - Swaps the position of temp_sort and swapper
+ * swap - Swaps two nodes in a doubly linked list
  *
- * @temp_sort: The first node to swap
- * @swapper: The second node to swap
- * @list: The list to be sorted
+ * @temp_sort: Auxiliary node
+ * @swapper: Auxiliary node
+ * @list: The doubly linked list to be sorted
  *
- * Return: the list with the new position of the nodes
- */
+ *Return: Returns a list
+*/
 listint_t **swap(listint_t *temp_sort, listint_t *swapper, listint_t **list)
 {
 	listint_t *aux_prev, *aux_next = temp_sort->next;
 
-	swapper = temp_sort->prev;
 	if (dlistint_len(*list) == 2)
 	{
 		temp_sort->next = temp_sort->prev;
@@ -101,20 +101,12 @@ listint_t **swap(listint_t *temp_sort, listint_t *swapper, listint_t **list)
 	return (list);
 }
 
-/**
- * dlistint_len - counts the number of nodes in a doubly linked list
- * @list: pointer to the head of the list
- *
- * Return: the number of nodes.
- */
 size_t dlistint_len(const listint_t *list)
 {
 	int n = 0;
 
 	if (list == NULL)
-	{
 		return (0);
-	}
 	while (list != NULL)
 	{
 		n++;
