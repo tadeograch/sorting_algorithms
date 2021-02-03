534f0313b1cd275ae870b5447dc5a1cd050a999f
Tadeo Grach <tade.g.17@gmail.com>
2021-02-03 13:26:40 +0000

    adding functions

--- sort.h
+++ sort.h
@@ -23,7 +23,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 
 listint_t **swap(listint_t *temp_sort, listint_t *swapper, listint_t **list);
